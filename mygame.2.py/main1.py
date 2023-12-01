c02cc5d65f6e011d17fe5ca5db41d52ce97a27a4
charlielongwello <142935137+charlielongwello@users.noreply.github.com>
2023-12-01 12:54:26 -0800

    Add files via upload

---
+++ mygame.2.py/main1.py
@@ -0,0 +1,8 @@
+# This file was created by: charlie longwello
+# The Final Game
+# my goal for my final project is to make a car game or basketball game
+# shoot the basketball into the hoop
+# its a game against AI
+# person with more points by the time the clock is up wins
+# or for the car game it will be a drifting game 
+# three laps around the track and try not to crash 
